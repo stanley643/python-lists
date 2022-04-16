e7f34c88dd11c46e8bcef6012618a8a945c54ad4
Stanley Murigi Njoroge <98167574+stanley643@users.noreply.github.com>
2022-04-16 22:58:22 +0300

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,16 @@
+primes = [2, 3,5,7]
+print(primes)
+print(primes[2])
+print(primes[1:3])
+
+planets = ['Mercury', 'Venus', 'Earth', 'Uranus', 'Neptune']
+print(planets[3])
+planets[3]= 'Venus'
+print(planets[3])
+
+hands = [
+    ['j','q','k'],
+    ['2','2','2'],
+    ['6','A','k'],
+]
+print(hands[2])
